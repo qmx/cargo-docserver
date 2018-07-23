c27629fec0e20d0019f36fb83ec93dd376a4ea75
Douglas Campos <qmx@qmx.me>
2018-07-23 12:23:45 +0000

    basic path handling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ extern crate tokio_io;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use std::io;
-use std::path::Path;
+use std::path::{Path,PathBuf};
 use std::str;
 use structopt::StructOpt;
 
@@ -28,26 +28,68 @@ enum Cargo {
 
 type ResponseFuture = Box<Future<Item = Response<Body>, Error = io::Error> + Send>;
 
+#[derive(Debug)]
+struct CrateInfo {
+    name: String,
+    doc_path: PathBuf,
+}
+
+impl CrateInfo {
+    fn parse() -> CrateInfo {
+        let meta = cargo_metadata::metadata(None).unwrap();
+        let package_name = &meta.packages[0].name;
+        let package_name_sanitized = str::replace(&package_name, "-", "_");
+        let doc_path = Path::new(&meta.target_directory)
+            .join("doc");
+        CrateInfo {
+            name: package_name_sanitized.clone(),
+            doc_path: doc_path.clone(),
+        }
+    }
+}
+
+#[test]
+fn test_make_relative() {
+    assert_eq!("foo/bar/baz", make_relative("/foo/bar/baz"));
+    assert_eq!("foo/bar/baz", make_relative("///foo/bar/baz"));
+}
+
+#[test]
+fn test_make_root_document() {
+    assert_eq!("/foo/hello/index.html", make_index("/foo/hello"));
+    assert_eq!("/foo/hello/index.html", make_index("/foo/hello/"));
+    assert_eq!("/foo/hello.foo", make_index("/foo/hello.foo"));
+}
+
+fn make_index(path: &str) -> String {
+    let sanitized_path = path.trim_right_matches("/");
+    if sanitized_path.contains(".") {
+        sanitized_path.to_string()
+    } else {
+        format!("{}/index.html", sanitized_path)
+    }
+}
+
+fn make_relative(path: &str) -> String {
+    path.trim_left_matches("/").to_string()
+}
+
 fn serve_docs(req: Request<Body>) -> ResponseFuture {
+    let info = CrateInfo::parse();
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Box::new(future::ok(
             Response::builder()
                 .status(302)
-                .header("Location", "/cargo_docserver")
+                .header("Location", format!("/{}", &info.name).as_str())
                 .body(Body::empty())
                 .unwrap(),
         )),
         (&Method::GET, path) => {
-            let meta = cargo_metadata::metadata(None).unwrap();
-            let package_name = &meta.packages[0].name;
-            let package_name_sanitized = str::replace(&package_name, "-", "_");
-            let doc_path = Path::new(&meta.target_directory)
-                .join("doc")
-                .join(package_name_sanitized);
-            eprintln!("{:?}, not found", &doc_path);
-
+            eprintln!("{:?}", info);
+            let full_path  = info.doc_path.join(&make_index(&make_relative(path)));
+            eprintln!("GET {:?}", full_path);
             Box::new(
-                tokio_fs::file::File::open("target/doc/cargo_docserver/index.html")
+                tokio_fs::file::File::open(full_path)
                     .and_then(|file| {
                         let buf: Vec<u8> = Vec::new();
                         tokio_io::io::read_to_end(file, buf)
